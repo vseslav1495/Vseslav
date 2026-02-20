4c8282bf2619e71434014653930a861f9166cd76
vseslav1495 <vseslavgrebenuk@gmail.com>
2026-02-20 18:14:39 +0200

    domashka
    domashka

---
+++ Domashka.py
@@ -0,0 +1,4 @@
+imennyk = input("Назви будь-який іменник▶️")
+dieslovo = input("Назви будь-яке діеслово▶️")
+prykmetnyk = input("Назви будь-який рикметник▶️")
+print("Одного разу", prykmetnyk, imennyk, "вирішив", dieslovo, "через паркан.")
